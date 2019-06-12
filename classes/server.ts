033ed0625734c6ef79eea5ce10f7ef3ebadab22e
Jorge Martínez <jorgeme0996@gmail.com>
2019-06-12 16:35:52 -0500

    Se sube hasta la clase 32

--- classes/server.ts
+++ classes/server.ts
@@ -32,6 +32,9 @@ export default class Server {
         this.io.on('connection', cliente =>{
             console.log('Cliente conectado');
 
+            // Mensajes
+            socket.mensaje(cliente, this.io);
+
             //Desconectar
             socket.desconectar(cliente);
         })
--- sockets/sockets.ts
+++ sockets/sockets.ts
@@ -5,3 +5,13 @@ export const desconectar = (cliente: Socket) => {
         console.log('Cliente Desconectado');
     });
 }
+
+//Escuchar mensajes
+export const mensaje = (cliente: Socket, io: SocketIO.Server) => {
+    cliente.on('mensaje', (payload: {de:string, cuerpo: string})=> {
+        console.log('Mensaje recibido', payload);
+
+        io.emit('mensaje-nuevo', payload);
+
+    })
+}
